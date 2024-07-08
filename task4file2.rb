b3c44daf5c865a09f576882ff8f25df7cfa3016d
Gleb Kabaev <glebkabaev200307@gmail.com>
2024-07-08 13:49:06 +0300

    almost complite

--- task4file2.rb
+++ task4file2.rb
@@ -1,4 +1,5 @@
 def where(age)
+  
   file=File.open("text.txt")
   file2=File.open("results.txt", "a")
   file_data=file.read.split("\n")
@@ -8,4 +9,32 @@ def where(age)
     end
   end
 end
-where(33)
+input=1000000000000000000
+while(input!=-1)
+  input=gets.chomp.to_i
+  where(input)
+  arr=[]
+  i=0
+  File.foreach("text.txt").with_index do |val,index1|
+    i=i+1
+    arr<<false
+    puts "\n"
+    puts arr
+    puts "\n"
+    File.foreach("results.txt").with_index do |val2,index2|
+      if val.include?(val2.to_s)
+      arr[index1]=true
+      end
+    end
+  end
+
+  flag=false
+  arr.each{|value|
+    if value==false
+      flag=true
+    end
+  }
+  if flag=false
+    input=-1
+  end
+end
