c74eded7f4f89a966580f39d80793a00f0a79264
Gleb Kabaev <glebkabaev200307@gmail.com>
2024-07-09 11:08:57 +0300

    full complete

--- task4file2.rb
+++ task4file2.rb
@@ -1,40 +1,36 @@
 def where(age)
   
-  file=File.open("text.txt")
-  file2=File.open("results.txt", "a")
-  file_data=file.read.split("\n")
+  #file=File.open("text.txt")
+  #file2=File.open("results.txt", "a")
+  #file_data=file.read.split("\n")
   File.foreach("text.txt").with_index do |val,index|
     if val.include?(age.to_s)
       File.write("results.txt",val.to_s, mode:"a")
     end
   end
 end
-input=1000000000000000000
-while(input!=-1)
-  input=gets.chomp.to_i
-  where(input)
-  arr=[]
-  i=0
-  File.foreach("text.txt").with_index do |val,index1|
-    i=i+1
-    arr<<false
-    puts "\n"
-    puts arr
-    puts "\n"
-    File.foreach("results.txt").with_index do |val2,index2|
-      if val.include?(val2.to_s)
-      arr[index1]=true
+input = 0
+  while input != -1
+    input = gets.chomp.to_i
+    break if input == -1
+  
+    where(input)
+  
+    all_written = true
+    text_lines = File.readlines("text.txt").map(&:chomp)
+    result_lines = File.readlines("results.txt").map(&:chomp)
+  
+    text_lines.each do |line|
+      unless result_lines.include?(line)
+        all_written = false
+        break
       end
     end
-  end
-
-  flag=false
-  arr.each{|value|
-    if value==false
-      flag=true
+  
+    if all_written
+      input = -1
     end
-  }
-  if flag=false
-    input=-1
   end
-end
+  
+  puts "Содержимое файла results.txt:"
+  File.foreach("results.txt") { |line| puts line }
